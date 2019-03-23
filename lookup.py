28d01cbd4f1a677d5950b4849e162284e4c0641f
John <jbertlik@gmail.com>
2019-03-22 23:13:51 -0400

    added some detials of what data I wanted from the JSON format. Also adding my own code to get the user to input the symbol and parse the code to find it.

--- lookup.py
+++ lookup.py
@@ -6,4 +6,20 @@ os.system('cls')
 api_request = requests.get("https://api.coinmarketcap.com/v1/ticker/")
 api = json.loads(api_request.content)
 
-print(api)
+currencies = ["BTC", "ETH", "XBY"]
+
+print("---------------------")
+for x in api:
+    for coin in currencies:
+        if coin == x["symbol"]:
+            print(x["name"])
+            print("${0:.2f}".format(float(x["price_usd"])))
+            print("Rank: {0:.0f}".format(float(x["rank"])))
+            print("---------------------")
+
+currency = input("Insert Crypto Coin Symbol: ")
+
+for y in api:
+    for symbol in currency:
+        if symbol == y["symbol"]:
+            print(y["symbol"])
