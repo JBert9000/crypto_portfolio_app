c0e8d35a1914f7375cfd43c3befbd98b8da80d54
John <jbertlik@gmail.com>
2019-03-25 16:03:46 -0400

    added a way to calculate profit and loss and what I 'bought' my coins at. I have some of my own code commented out that doesn't work properly uyet. I will get back to that and fix it after I complete the tutorial.

--- lookup.py
+++ lookup.py
@@ -3,23 +3,75 @@ import json
 import os
 os.system('cls')
 
+coin_names = ["", "xtrabytes", "vestchain", "dash"]
+
 api_request = requests.get("https://api.coinmarketcap.com/v1/ticker/")
 api = json.loads(api_request.content)
 
-currencies = ["BTC", "ETH", "XBY"]
+# api_request = requests.get("https://api.coinmarketcap.com/v1/ticker/" + str(coin_names))
+# api = json.loads(api_request.content)
+
+# xby_request = requests.get("https://api.coinmarketcap.com/v1/ticker/xtrabytes")
+# xby = json.loads(xby_request.content)
+
+
+
+# My portfolio
+my_portfolio = [
+    {
+        "sym": "ETH",
+        "amount_owned": 0.8,
+        "price_paid_per": 500
+    },
+    {
+        "sym": "XBY",
+        "amount_owned": 2500,
+        "price_paid_per": 0.23
+    },
+    {
+        "sym": "VEST",
+        "amount_owned": 500,
+        "price_paid_per": 0.004
+    },
+    {
+        "sym": "DASH",
+        "amount_owned": 1000,
+        "price_paid_per": 10
+    }
+]
 
 print("---------------------")
+
+# for i in coin_names:
+#     for coin_names in api_request:
+#         print(i)
+
+portfolio_profit_loss = 0
+
 for x in api:
-    for coin in currencies:
-        if coin == x["symbol"]:
+    for coin in my_portfolio:
+        if coin["sym"] == x["symbol"]:
+
+            # Do some math
+            total_paid = coin["amount_owned"] * coin["price_paid_per"]
+            current_value = float(coin["amount_owned"]) * float(x["price_usd"])
+            profit_loss = current_value - total_paid
+            portfolio_profit_loss += profit_loss
+
             print(x["name"])
             print("${0:.2f}".format(float(x["price_usd"])))
             print("Rank: {0:.0f}".format(float(x["rank"])))
+            print("Total Paid: ${0:.2f}".format(float(total_paid)))
+            print("Current Value: ${0:.2f}".format(current_value))
+            print("Profit / Loss: ${0:.2f}".format(profit_loss))
             print("---------------------")
 
-currency = input("Insert Crypto Coin Symbol: ")
+print("GAINZ = ${0:.2f}".format(float(portfolio_profit_loss)))
 
-for y in api:
-    for symbol in currency:
-        if symbol == y["symbol"]:
-            print(y["symbol"])
+# currency = input("Insert Crypto Coin Symbol: ")
+#
+# for y in api:
+#     for symbol in currency:
+#         if symbol == y["symbol"]:
+#             print(y["symbol"])
+
